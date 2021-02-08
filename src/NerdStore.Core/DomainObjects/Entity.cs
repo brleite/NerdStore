f08a16b562833ba0aa9cbeed49da4bd0f2e8a7a4
Bruno Mayerle Leite <brleite@gmail.com>
2021-02-08 15:15:48 -0300

    Criação das primeiras entidades

---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var entity = obj as Entity;
+
+            if (ReferenceEquals(this, entity))
+                return true;
+            if (ReferenceEquals(null, entity))
+                return false;
+
+            return Id.Equals(entity.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+    }
+}

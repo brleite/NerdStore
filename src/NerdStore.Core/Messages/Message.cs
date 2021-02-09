7695be5593e84d87c810b0e6f0124530762c3945
Bruno Mayerle Leite <brleite@gmail.com>
2021-02-09 10:53:29 -0300

    Domain Service e Evento

---
+++ src/NerdStore.Core/Messages/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NerdStore.Core.Messages
+{
+    public class Message
+    {
+        public string MessageType { get; protected set; }
+        public Guid AggredateId { get; protected set; }
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}

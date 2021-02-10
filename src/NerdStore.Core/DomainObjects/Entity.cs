d1ffe0b3e7479ee4a06c7eb12e586139ae671a88
Bruno Mayerle Leite <brleite@gmail.com>
2021-02-10 15:29:03 -0300

    Início do contexto de Vendas

--- src/NerdStore.Core/DomainObjects/Entity.cs
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -1,42 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using NerdStore.Core.Messages;
 
 namespace NerdStore.Core.DomainObjects
 {
     public abstract class Entity
     {
         public Guid Id { get; set; }
+        private List<Event> _notificacoes;
+        public IReadOnlyCollection<Event> Notificacoes => _notificacoes?.AsReadOnly();
 
         protected Entity()
         {
             Id = Guid.NewGuid();
         }
+        
+        public void AdicionarEvento(Event evento)
+        {
+            _notificacoes ??= new List<Event>();
+            _notificacoes.Add(evento);
+        }
+
+        public void RemoverEvento(Event evento)
+        {
+            _notificacoes?.Remove(evento);
+        }
+
+        public void LimparEventos()
+        {
+            _notificacoes?.Clear();
+        }
 
         public override bool Equals(object obj)
         {
-            var entity = obj as Entity;
+            var compareTo = obj as Entity;
 
-            if (ReferenceEquals(this, entity))
-                return true;
-            if (ReferenceEquals(null, entity))
-                return false;
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
 
-            return Id.Equals(entity.Id);
-        }
-
-        public override int GetHashCode()
-        {
-            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+            return Id.Equals(compareTo.Id);
         }
 
         public static bool operator ==(Entity a, Entity b)
         {
-            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
-                return true;
-
-            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
-                return false;
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null)) return true;
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
 
             return a.Equals(b);
         }
@@ -46,9 +54,19 @@ namespace NerdStore.Core.DomainObjects
             return !(a == b);
         }
 
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
         public override string ToString()
         {
             return $"{GetType().Name} [Id={Id}]";
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class PedidoItem : Entity
+    {
+        public Guid PedidoId { get; private set; }
+
+        // Como só utilizamos a propriedade ProdutoNome, não precisamos criar uma nova classe Produto nesse contexto. Caso fossem mais propriedades,
+        // valeria a pena criar uma nova Entidade.
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        
+        // EF Rel.
+        public Pedido Pedido { get; set; }
+
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        // Necessário para o EF
+        protected PedidoItem() { }
+
+        internal void AssociarPedido(Guid pedidoId)
+        {
+            PedidoId = pedidoId;
+        }
+
+        public decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal void AtualizarUnidades(int unidades)
+        {
+            Quantidade = unidades;
+        }
+
+        public override bool EhValido()
+        {
+            return true;
+        }
+    }
+}

bf1049219f2342bd6e15a2646e83592c2493edda
Bruno Mayerle Leite <brleite@gmail.com>
2021-02-10 09:42:55 -0300

    Scripts de inicialização

---
+++ src/NerdStore.Catalogo.Data/Scripts/InitCategorias.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM NerdStoreDb.Categorias
+LIMIT 0, 1000
+
+-- Date: 2021-02-10 09:42
+*/
+INSERT INTO `` (`Id`,`Nome`,`Codigo`) VALUES ('e093ac4d-4142-4790-a3b1-a402a1fd2dc4','Camiseta',1);
---
+++ src/NerdStore.Catalogo.Data/Scripts/InitProdutos.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM NerdStoreDb.Produtos
+LIMIT 0, 1000
+
+-- Date: 2021-02-10 09:41
+*/
+INSERT INTO `` (`Id`,`CategoriaId`,`Nome`,`Descricao`,`Ativo`,`Valor`,`DataCadastro`,`Imagem`,`QuantidadeEstoque`,`Altura`,`Largura`,`Profundidade`) VALUES ('08d8cdbf-a1e5-4c6e-edb6-d01ad0e08a57','e093ac4d-4142-4790-a3b1-a402a1fd2dc4','Camiseta Show de Bola','Teste de Camiseta','1',12.000000000000000000000000000000,'2021-02-10 09:30:21.397548','camiseta1.jpg',50,1,1,1);
+INSERT INTO `` (`Id`,`CategoriaId`,`Nome`,`Descricao`,`Ativo`,`Valor`,`DataCadastro`,`Imagem`,`QuantidadeEstoque`,`Altura`,`Largura`,`Profundidade`) VALUES ('08d8cdbf-bbce-d989-3d9b-f79470b2ea6a','e093ac4d-4142-4790-a3b1-a402a1fd2dc4','Camiseta 2','Camiseta 2','1',40.000000000000000000000000000000,'2021-02-10 09:31:04.627236','camiseta2.jpg',40,10,10,10);
